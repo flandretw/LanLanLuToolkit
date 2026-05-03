e823041b59b91981cb40c8218e4814f73ab7f7f9
蘭蘭露 <51469621+flandretw@users.noreply.github.com>
2026-05-03 16:34:38 +0800

    feat: implement HomePage with localized greetings and version display

--- lanlanlu-toolkit/Views/HomePage.xaml.cs
+++ lanlanlu-toolkit/Views/HomePage.xaml.cs
@@ -8,6 +8,41 @@ namespace lanlanlu_toolkit.Views
         public HomePage()
         {
             this.InitializeComponent();
+            UpdateGreeting();
+            UpdateVersion();
+        }
+
+        private void UpdateVersion()
+        {
+            var loader = new Microsoft.Windows.ApplicationModel.Resources.ResourceLoader();
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var version = assembly.GetName().Version;
+            
+            if (version != null)
+            {
+                // 格式化為 Major.Minor.Build
+                string versionStr = $"{version.Major}.{version.Minor}.{version.Build}";
+                string format = loader.GetString("HomePage_Version_Format");
+                VersionText.Text = string.Format(format, versionStr);
+            }
+        }
+
+        private void UpdateGreeting()
+        {
+            var loader = new Microsoft.Windows.ApplicationModel.Resources.ResourceLoader();
+            var hour = System.DateTime.Now.Hour;
+            string resourceKey;
+
+            if (hour >= 23 || hour < 5)
+                resourceKey = "Greeting_LateNight";
+            else if (hour >= 5 && hour < 11)
+                resourceKey = "Greeting_Morning";
+            else if (hour >= 11 && hour < 17)
+                resourceKey = "Greeting_Afternoon";
+            else // 17 - 23
+                resourceKey = "Greeting_Evening";
+
+            GreetingText.Text = loader.GetString(resourceKey);
         }
 
         private void GoToPerformance_Click(object sender, RoutedEventArgs e)
